8a67529c44caa7af736eee54ca26acb1cd52b4cd
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-25 16:46:35 +0100

    html css structure

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// stampa in console i numeri da 1 a 100
+// se il numero è multiplo di 5 e di 3 stampa "fizzbuzz"
+// se il numero è multiplo di 5 stampa "buzz"
+// se il numero è multiplo di 3 stampa "fizz"
+
