27d03f04c1e7ce83009093fb4c55e0a0a48d1abd
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-25 17:17:17 +0100

    js working

--- js/main.js
+++ js/main.js
@@ -2,4 +2,30 @@
 // se il numero è multiplo di 5 e di 3 stampa "fizzbuzz"
 // se il numero è multiplo di 5 stampa "buzz"
 // se il numero è multiplo di 3 stampa "fizz"
+// altrimenti stampa il numero
 
+
+
+
+for (let i = 1; i <= 100; i++) {
+
+    // se il numero è multiplo di 5 e di 3 stampa "fizzbuzz"
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    }
+
+    // se il numero è multiplo di 5 stampa "buzz"
+    else if (i % 5 == 0) {
+        console.log('Buzz')
+    }
+
+    // se il numero è multiplo di 3 stampa "fizz"
+    else if (i % 3 == 0) {
+        console.log('Fizz')
+    }
+
+    // altrimenti stampa il numero
+    else {
+        console.log(i)
+    }
+}
